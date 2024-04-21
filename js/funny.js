dcca3d349618b17433a11ec2f26badfc0101bc8a
skibidimaster1 <167704981+skibidimaster1@users.noreply.github.com>
2024-04-21 22:22:06 +0100

    Update funny.js

--- js/funny.js
+++ js/funny.js
@@ -4,28 +4,28 @@ const body = document.getElementsByTagName('body');
 window.addEventListener("keydown", (event) => {
   if(event.ctrlKey && (event.key === "S" || event.key === "s")) {
      event.preventDefault();
-     body[0].innerHTML = "MADE BY PK X RESTRICT NICE TRY."
+     body[0].innerHTML = "MADE BY GST X RESTRICT NICE TRY."
   }
 
   if(event.ctrlKey && (event.key === "C")) {
      event.preventDefault();
-     body[0].innerHTML = "MADE BY PK X RESTRICT NICE TRY."
+     body[0].innerHTML = "MADE BY GST X RESTRICT NICE TRY."
   }
   if(event.ctrlKey && (event.key === "E" || event.key === "e")) {
      event.preventDefault();
-     body[0].innerHTML = "MADE BY PK X RESTRICT NICE TRY."
+     body[0].innerHTML = "MADE BY GST X RESTRICT NICE TRY."
   }
   if(event.ctrlKey && (event.key === "I" || event.key === "i" || event.key === "Ä±")) {
      event.preventDefault();
-     body[0].innerHTML = "MADE BY PK X RESTRICT NICE TRY.";
+     body[0].innerHTML = "MADE BY GST X RESTRICT NICE TRY."
   }
   if(event.ctrlKey && (event.key === "K" || event.key === "k")) {
      event.preventDefault();
-     body[0].innerHTML = "MADE BY PK X RESTRICT NICE TRY.";
+     body[0].innerHTML = "MADE BY GST X RESTRICT NICE TRY."
   }
   if(event.ctrlKey && (event.key === "U" || event.key === "u")) {
      event.preventDefault();
-     body[0].innerHTML = "MADE BY PK X RESTRICT NICE TRY.";
+     body[0].innerHTML = "MADE BY GST X RESTRICT NICE TRY."
   }
 });
 // stop right click
