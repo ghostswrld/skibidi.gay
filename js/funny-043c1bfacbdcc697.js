d4ea801ceb90a62d0e54b9375cb1d484100c6543
ghost <142125154+ghostswrld@users.noreply.github.com>
2024-05-25 22:35:25 +0100

    Add files via upload

---
+++ js/funny-043c1bfacbdcc697.js
@@ -0,0 +1,175 @@
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[658],{
+
+/***/ 4242:
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/funny",
+      function () {
+        return __webpack_require__(2417);
+      }
+    ]);
+    if(false) {}
+  
+
+/***/ }),
+
+/***/ 2417:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "__N_SSP": function() { return /* binding */ __N_SSP; }
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9008);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_Navbar__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(2356);
+
+
+
+var Funny = function(param) {
+    var ip = param.ip, IPInfo = param.IPInfo;
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "flex min-h-screen flex-col",
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)((next_head__WEBPACK_IMPORTED_MODULE_1___default()), {
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("title", {
+                        children: "gst"
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("meta", {
+                        name: "title",
+                        content: "gst"
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("meta", {
+                        name: "description",
+                        content: "gsts website"
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("meta", {
+                        name: "keywords",
+                        content: "gsts website - skibidi.gay"
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("link", {
+                        rel: "icon",
+                        href: "/favicon.ico"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_components_Navbar__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z, {}),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
+                className: "relative w-full",
+                style: {
+                    "minHeight": "60vh",
+                    "top": "-25px",
+                    "backgroundImage": "linear-gradient(0deg, transparent, rgb(39 39 42))",
+                    "zIndex": "1"
+                }
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
+                className: "flex justify-center relative text-justify",
+                style: {
+                    "height": "800px"
+                },
+                children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
+                    className: "relative animate-crawl",
+                    style: {
+                        "top": "99999px",
+                        "transformOrigin": "50% 100%"
+                    },
+                    children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: "flex flex-col gap-14",
+                        children: [
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                className: "w-fit text-3xl mr-5 text-slate-300 break-all",
+                                children: [
+                                    "IP: ",
+                                    ip
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                className: "w-fit text-3xl mr-5 text-slate-300 break-all",
+                                children: [
+                                    "Continent: ",
+                                    IPInfo.continent
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                className: "w-fit text-3xl mr-5 text-slate-300 break-all",
+                                children: [
+                                    "Country: ",
+                                    IPInfo.country
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                className: "w-fit text-3xl mr-5 text-slate-300 break-all",
+                                children: [
+                                    "Region: ",
+                                    IPInfo.regionName
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                className: "w-fit text-3xl mr-5 text-slate-300 break-all",
+                                children: [
+                                    "City: ",
+                                    IPInfo.city
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                className: "w-fit text-3xl mr-5 text-slate-300 break-all",
+                                children: [
+                                    "Zip Code: ",
+                                    IPInfo.zip
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                className: "w-fit text-3xl mr-5 text-slate-300 break-all",
+                                children: [
+                                    "Latitude: ",
+                                    IPInfo.lat
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                className: "w-fit text-3xl mr-5 text-slate-300 break-all",
+                                children: [
+                                    "Longitude: ",
+                                    IPInfo.lon
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                className: "w-fit text-3xl mr-5 text-slate-300 break-all",
+                                children: [
+                                    "ISP: ",
+                                    IPInfo.isp
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                className: "w-fit text-3xl mr-5 text-slate-300 break-all",
+                                children: [
+                                    "Timezone: ",
+                                    IPInfo.timezone
+                                ]
+                            })
+                        ]
+                    })
+                })
+            })
+        ]
+    });
+};
+var __N_SSP = true;
+/* harmony default export */ __webpack_exports__["default"] = (Funny);
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, [107,774,888,179], function() { return __webpack_exec__(4242); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
